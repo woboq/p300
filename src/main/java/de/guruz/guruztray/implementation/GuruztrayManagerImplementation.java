f44d4d74d53aa8992603e27e55116216bb8f173f
Frank Dietrich <bits_n_bytes@gmx.de>
2014-10-22 13:20:59 +0200

    disable system tray on Gnome desktop

--- src/main/java/de/guruz/guruztray/implementation/GuruztrayManagerImplementation.java
+++ src/main/java/de/guruz/guruztray/implementation/GuruztrayManagerImplementation.java
@@ -38,6 +38,7 @@
 */
 package de.guruz.guruztray.implementation;
 
+import de.guruz.guruztray.interfaces.GuruztrayManager;
 import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.PopupMenu;
@@ -46,30 +47,35 @@ import java.awt.TrayIcon;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-import de.guruz.guruztray.interfaces.GuruztrayManager;
-
 public class GuruztrayManagerImplementation implements GuruztrayManager, MouseListener {
 
 	protected SystemTray tray = null;
 	protected TrayIcon trayIcon = null;
-	
+
 	public GuruztrayManagerImplementation () throws Exception {
+        // workaround to disable system tray in Gnome 3
+        //
+        // see following comment for an explanation
+        // https.//bugzilla.redhat.com/show_bug.cgi?id=683768#c4
+        if ("gnome".equals(System.getenv("XDG_SESSION_DESKTOP"))) {
+            throw new Exception("System tray support has been disabled for Gnome desktop environment");
+        }
 		if (!isSupported())
 			throw new Exception ("Tray not supported");
-		
+
 		tray = SystemTray.getSystemTray();
-		
+
 		Dimension d = tray.getTrayIconSize();
 		//System.out.println ("System Tray icon width=" + d.width + " height=" + d.height);
-		
+
 	}
-	
-	
-	
+
+
+
 	public void setTrayIcon(Image i_16x16, Image i_22x22, String tt) throws Exception {
 		setTrayIcon (i_16x16, i_22x22, tt, null);
 	}
-	
+
 	public void setTrayIcon(Image i_16x16, Image i_22x22, String tt, PopupMenu pm) throws Exception {
 		boolean autoSize = false;
 		Image i = null;
@@ -86,18 +92,18 @@ public class GuruztrayManagerImplementation implements GuruztrayManager, MouseLi
 			//System.out.println ("Tray 22x22 picked");
 			autoSize = true;
 		}
-			
+
 		trayIcon = new TrayIcon (i, tt);
 		trayIcon.addMouseListener(this);
 		trayIcon.setImageAutoSize(autoSize);
-		
+
 		if (pm != null)
 			trayIcon.setPopupMenu(pm);
-		
+
 		tray.add(trayIcon);
-		
+
 		//System.out.println ("Tray real width=" + trayIcon.getSize().width + " height=" + trayIcon.getSize().height);
-		
+
 	}
 
 	public boolean isSupported() {
@@ -105,27 +111,27 @@ public class GuruztrayManagerImplementation implements GuruztrayManager, MouseLi
 	}
 	public void mouseClicked(MouseEvent arg0) {
 		//System.out.println ("Clicked");
-		
+
 	}
 	public void mouseEntered(MouseEvent arg0) {
 		//System.out.println ("Entered");
-		
+
 	}
 	public void mouseExited(MouseEvent arg0) {
 		//System.out.println ("Exited");
-		
+
 	}
-	
+
 	/**
 	 * If it is the left mouse button, show/hide the associated window
 	 */
 	public void mousePressed(MouseEvent arg0) {
-		
+
 		if (window != null && (arg0.getButton() == MouseEvent.BUTTON1)) {
 			// do not do that on OSX :)
 			if (isOSX ())
 				return;
-			
+
 			//System.out.println ("Pressed left button");
 			boolean v = window.isVisible();
 			if (v) {
@@ -135,24 +141,24 @@ public class GuruztrayManagerImplementation implements GuruztrayManager, MouseLi
 				window.requestFocus();
 				window.toFront ();
 			}
-				
-		
-			
+
+
+
 		}
-			
-		
+
+
 	}
 	public void mouseReleased(MouseEvent arg0) {
 		//System.out.println ("Released");
-		
+
 	}
-	
+
 	protected java.awt.Window window = null;
-	
+
 	public void setAssociatedWindow (java.awt.Window w) {
 		window = w;
 	}
-	
+
 	protected boolean isOSX() {
 		return (System.getProperty("os.name").startsWith("Mac"));
 	}
